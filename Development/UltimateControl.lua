7be5cb81f11ca5f8f84713e0e3fbc00688265535
TAJG <that.joshua.green@gmail.com>
2013-03-20 13:25:17 +0000

    adding RD code

--- Development/UltimateControl.lua
+++ Development/UltimateControl.lua
@@ -6,6 +6,10 @@ local ChatLib = loadLibrary("chat")
 local EntityLib = loadLibrary("ents")
 
 WireLib.createInputs({"ResourceCache","LSCore","WaterSplitter","WaterPump"},{"wirelink","wirelink","wirelink","wirelink"})
+CA = WireLib.ports.ResourceCache
+CR = WireLib.ports.LSCore
+WS = WireLib.ports.WaterSplitter
+WP = WireLib.ports.WaterPump
 
 -- Modules
 LS = false -- LSCore, WaterSplitter, WaterPump operations {REQUIRES RD TO WORK}
@@ -14,8 +18,7 @@ SG = false -- Stargate and ring stuff
 
 
 function getPercent(Value1, Value2)
-	local Total = Value1 / Value2 * 100
-	return Total
+	return (Value1 / Value2) * 100
 end
 
 
@@ -29,10 +32,49 @@ function LifeSupportModule()
 
 end
 
-function ResourceMontoringModule()
-	do Resource stuff...
-
+function ResourceAmmountGet(resource)
+	if resource == "Oxygen" then
+		local Value1 = CA.oxygen
+		local Value2 = CA.["Max oxygen"]
+		return getPercent(Value1, Value2)
+	end
+	if resource == "Power" then
+		local Value1 = CA.energy
+		local Value2 = CA.["Max energy"]
+		return getPercent(Value1, Value2)
+	end
+	if resource == "Water" then
+		local Value1 = CA.water
+		local Value2 = CA.["Max water"]
+		return getPercent(Value1, Value2)
+	end
 end
+
+function RDStatus()
+	if O2 <= 15 and > 5 then
+		print(string.format("You are running low on oxygen, you have %i percent left", O2))
+	elseif O2 <= 5 and > 0 then
+		print(string.format("You are running out of oxygen, you have %i percent left", O2))
+	elseif O2 == 0 then
+		print("You are out of oxygen")
+	else print("Oxygen is OK!")
+	end
+	if PWR <= 15 and > 5 then
+		print(string.format("You are running low on power, you have %i percent left", PWR))
+	elseif PWR <= 5 and > 0 then
+		print(string.format("You are running out of power, you have %i percent left", PWR))
+	elseif PWR == 0 then
+		print("You are out of power")
+	else print("Oxygen is OK!")
+	end
+	if WTR <= 15 and > 5 then
+		print(string.format("You are running low on water, you have %i percent left", WTR))
+	elseif WTR <= 5 and > 0 then
+		print(string.format("You are running out of water, you have %i percent left", WTR))
+	elseif WTR == 0 then
+		print("You are out of water")
+	else print("Oxygen is OK!")
+	end
 
 
 -- Loading of moduals for hooking
@@ -41,5 +83,9 @@ if LS = true then
 end
 
 if RD = true then
-	hook("input","ResourceDistrobution", ResourceMontoringModule() )
+	hook("think","ResourceDistribution", function()
+		O2 = ResourceAmmountGet("Oxygen")
+		PWR = ResourceAmmountGet("Power")
+		WTR = ResourceAmmountGet("Water")
+		
 end 
